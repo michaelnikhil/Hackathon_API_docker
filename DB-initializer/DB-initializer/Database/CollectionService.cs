b26c0334e4ffe9d1b55a7a4caa2577f697428fe5
michaeldescamps <michaeldescamps@live.comgit config --global user.email michaeldescamps@live.comgit config --global credential.helper git config --global user.name michaeldescampsgit config --global user.email michael.n.descamps@gmail.com>
2021-07-06 22:08:50 +0200

    Condition if collection already exists

--- DB-initializer/DB-initializer/Database/CollectionService.cs
+++ DB-initializer/DB-initializer/Database/CollectionService.cs
@@ -1,6 +1,8 @@
 
 using System.Threading.Tasks;
 using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace DB_initializer.Database
 {
@@ -17,8 +19,20 @@ namespace DB_initializer.Database
         public async Task<bool> CreateCollection()
         {
             Console.WriteLine("*** create collection ***");
-            await _context.Database.CreateCollectionAsync(_context.CollectionName);
+            if(!await CollectionExists(_context.CollectionName)){
+                await _context.Database.CreateCollectionAsync(_context.CollectionName);
+            }
+            else{
+                Console.WriteLine("Collection already exists");
+            }
             return true;
+        }
+
+        public async Task<bool> CollectionExists(string collectionName)
+        {
+            var filter = new BsonDocument("name", collectionName);
+            IAsyncCursor<BsonDocument> collections = await _context.Database.ListCollectionsAsync(new ListCollectionsOptions { Filter = filter });
+            return await collections.AnyAsync();
         }
     }
 }
--- DB-initializer/DB-initializer/Database/ICollectionService.cs
+++ DB-initializer/DB-initializer/Database/ICollectionService.cs
@@ -5,5 +5,8 @@ namespace DB_initializer.Database
     public interface ICollectionService
     {
         Task<bool> CreateCollection();
+
+        Task<bool> CollectionExists(string collectionName);
+        
     }
 }
