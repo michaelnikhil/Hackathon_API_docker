b81aabe0d72d43b6c33207ae9fdcdd409d61b27a
michaeldescamps <michaeldescamps@live.comgit config --global user.email michaeldescamps@live.comgit config --global credential.helper git config --global user.name michaeldescampsgit config --global user.email michael.n.descamps@gmail.com>
2021-07-06 21:29:56 +0200

    Create a new database with a collection

--- DB-initializer/DB-initializer/Program.cs
+++ DB-initializer/DB-initializer/Program.cs
@@ -7,6 +7,7 @@ using MongoDB.Bson;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using DB_initializer.Database;
+using DB_initializer.Job;
 
 namespace DB_initializer
 {
@@ -16,33 +17,50 @@ namespace DB_initializer
 
         static void Main(string[] args)
         {
+            string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
             Console.WriteLine("Hello World!");
             Console.WriteLine("--- Mongo DB initialise ---");
+    
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .AddCommandLine(args);
+               
+            Log.Logger = new LoggerConfiguration() // initiate the logger configuration
+                            .ReadFrom.Configuration(builder.Build()) // connect serilog to our configuration folder
+                            .Enrich.FromLogContext() //Adds more information to our logs from built in Serilog 
+                            .WriteTo.Console() // decide where the logs are going to be shown
+                            .WriteTo.Debug()
+                            .CreateLogger(); //initialise the logger
+
+            Log.Logger.Information("Application Starting");
 
             BsonSerializer.RegisterSerializer(typeof(decimal), new DecimalSerializer(BsonType.Decimal128));
             BsonSerializer.RegisterSerializer(typeof(decimal?), new NullableSerializer<decimal>(new DecimalSerializer(BsonType.Decimal128)));
 
+
             Configuration = builder.Build();
 
             var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging();
             ConfigureServices(serviceCollection);
 
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            serviceProvider.GetService<RunTasks>().Run().Wait();
+            Log.Logger.Information("Import finished");
         }
 
         private static void ConfigureServices(IServiceCollection services)
         {
+            Log.Logger.Information("Configure services");
             services
-                .AddLogging(configure => configure.AddSerilog())
-                .AddTransient<IMongoDbContext, MongoDbContext>()
-                .AddTransient(typeof(ICollectionService<>), typeof(CollectionService<>));
-
-            services
-                 .Configure<MongoDbSettings>(Configuration.GetSection("MongoDb"));
+            .Configure<MongoDbSettings>(options => Configuration.GetSection("MongoDbSettings").Bind(options))
+            .AddTransient<IMongoDbContext,MongoDbContext>()
+            .AddTransient(typeof(ICollectionService),typeof(CollectionService))
+            .AddTransient<RunTasks>();
         }
+
     }
 }
